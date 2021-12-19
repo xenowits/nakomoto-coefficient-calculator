7bbdc206c43054744fc3daae025f0b1a881465aa
Abhishek Kumar <xenowits@Abhisheks-MacBook-Pro.local>
2021-12-19 14:09:31 +0530

    feat: add calc for binance smart chain

--- utils/calc_nakamoto_coefficient.go
+++ utils/calc_nakamoto_coefficient.go
@@ -1,7 +1,7 @@
 package utils
 
 func CalcNakamotoCoefficient(totalVotingPower int64, votingPowers []int64) int {
-	var cumulativePercent, thresholdPercent, curr float64 = 0.00, 33.33, 0.00
+	var cumulativePercent, thresholdPercent, curr float64 = 0.00, THRESHOLD_PERCENT, 0.00
 	nakamotoCoefficient := 0
 	for _, vp := range votingPowers {
 		curr = float64(vp) / float64(totalVotingPower)
---
+++ utils/constants.go
@@ -0,0 +1,3 @@
+package utils
+
+const THRESHOLD_PERCENT float64 = 33.00
