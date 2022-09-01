fdbf950b974a4b6c4a5959343efee7877585541f
Rachel Franks <nfranks@protonmail.com>
2022-09-01 09:26:59 -0500

    address review comments

--- core/chains/nano.go
+++ core/chains/nano.go
@@ -12,6 +12,48 @@ import (
 	"sort"
 )
 
+type nanoOnlineRequest struct {
+	Action string `json:"action"`
+}
+
+type nanoOnlineResponse struct {
+	OnlineStake string `json:"trended_stake_total,omitempty"`
+	Error       string `json:"error,omitempty"` // nano nodes return status 200 even when erroring
+}
+
+type nanoRepsRequest struct {
+	Action  string `json:"action"`
+	Sorting bool   `json:"sorting"`
+	Count   int    `json:"count"`
+}
+
+type nanoRepsResponse struct {
+	Reps  map[string]string `json:"representatives,omitempty"`
+	Error string            `json:"error,omitempty"`
+}
+
+type nanoRichRequest struct{}
+
+type nanoRichAccount struct {
+	Balance string `json:"balance"`
+	Rep     string `json:"rep"`
+}
+
+type nanoRichIdentity struct {
+	Identity string                     `json:"identity"`
+	Accounts map[string]nanoRichAccount `json:"accounts"`
+}
+
+type nanoRichResponse struct {
+	Richlist []nanoRichIdentity `json:"richlist,omitempty"`
+	Error    string             `json:"error,omitempty"`
+}
+
+type nanoEntity struct {
+	Name   string
+	Weight *big.Int
+}
+
 func nanoRequest[ReqType any, ResType any](url string, req ReqType) (*ResType, error) {
 	var response ResType
 	body, err := json.Marshal(req)
@@ -36,47 +78,41 @@ func Nano() (int, error) {
 	rpc := "http://localhost:7076"
 	richlist := "https://nano.nendly.com/nakamoto/richlist"
 
-	bigFromStr := func(value string) *big.Int {
-		bigValue, _ := new(big.Int).SetString(value, 10)
-		return bigValue
+	bigFromStr := func(value string) (*big.Int, error) {
+		bigValue, ok := new(big.Int).SetString(value, 10)
+		if !ok {
+			return nil, fmt.Errorf("failed to parse big int from %v", value)
+		}
+		return bigValue, nil
 	}
 
 	// Determine the amount of nano that's needed for an attack
-	type OnlineRequest struct {
-		Action string `json:"action"`
-	}
-	type OnlineResponse struct {
-		OnlineStake string `json:"trended_stake_total,omitempty"`
-		Error       string `json:"error,omitempty"` // nano nodes return status 200 even when erroring
-	}
-	onlineRes, err := nanoRequest[OnlineRequest, OnlineResponse](rpc, OnlineRequest{
+	onlineRes, err := nanoRequest[nanoOnlineRequest, nanoOnlineResponse](rpc, nanoOnlineRequest{
 		Action: "confirmation_quorum",
 	})
+
 	if onlineRes != nil && onlineRes.Error != "" {
 		log.Println(onlineRes.Error)
-		return -1, errors.New("failed to get the online stake")
+		return -1, errors.New("failed to get online stake")
 	}
 	if err != nil {
 		return -1, err
 	}
-	online := bigFromStr(onlineRes.OnlineStake)
+
+	online, err := bigFromStr(onlineRes.OnlineStake)
+	if err != nil {
+		return -1, err
+	}
+
 	needed := new(big.Int).Div(online, big.NewInt(3))
 
 	// Request the list of reps
-	type RepsRequest struct {
-		Action  string `json:"action"`
-		Sorting bool   `json:"sorting"`
-		Count   int    `json:"count"`
-	}
-	type RepsResponse struct {
-		Reps  map[string]string `json:"representatives,omitempty"`
-		Error string            `json:"error,omitempty"`
-	}
-	repsRes, err := nanoRequest[RepsRequest, RepsResponse](rpc, RepsRequest{
+	repsRes, err := nanoRequest[nanoRepsRequest, nanoRepsResponse](rpc, nanoRepsRequest{
 		Action:  "representatives",
 		Sorting: true,
 		Count:   100,
 	})
+
 	if repsRes != nil && repsRes.Error != "" {
 		log.Println(repsRes.Error)
 		return -1, errors.New("failed to get the list of representatives")
@@ -84,27 +120,20 @@ func Nano() (int, error) {
 	if err != nil {
 		return -1, err
 	}
-	reps := make(map[string]*big.Int)
-	for rep, weight := range repsRes.Reps {
-		bigWeight := bigFromStr(weight)
-		reps[rep] = bigWeight
+
+	validators := make(map[string]*big.Int)
+	for validator, weight := range repsRes.Reps {
+		bigWeight, err := bigFromStr(weight)
+		if err != nil {
+			return -1, err
+		}
+
+		validators[validator] = bigWeight
 	}
 
 	// Request the "richlist" of high-value accounts
-	type RichRequest struct{}
-	type RichAccount struct {
-		Balance string `json:"balance"`
-		Rep     string `json:"rep"`
-	}
-	type RichIdentity struct {
-		Identity string                 `json:"identity"`
-		Accounts map[string]RichAccount `json:"accounts"`
-	}
-	type RichResponse struct {
-		Richlist []RichIdentity `json:"richlist,omitempty"`
-		Error    string         `json:"error,omitempty"`
-	}
-	richRes, err := nanoRequest[RichRequest, RichResponse](richlist, RichRequest{})
+	richRes, err := nanoRequest[nanoRichRequest, nanoRichResponse](richlist, nanoRichRequest{})
+
 	if richRes != nil && richRes.Error != "" {
 		log.Println(richRes.Error)
 		return -1, errors.New("failed to get the list of representatives")
@@ -119,11 +148,15 @@ func Nano() (int, error) {
 
 		// move all of this identity's nano off the reps
 		for _, account := range group.Accounts {
-			balance := bigFromStr(account.Balance)
-			rep := account.Rep
+			balance, err := bigFromStr(account.Balance)
+			if err != nil {
+				return -1, err
+			}
+
+			representative := account.Rep
 			power = power.Add(power, balance)
 
-			prior, ok := reps[rep]
+			prior, ok := validators[representative]
 			if !ok {
 				continue
 			}
@@ -131,40 +164,35 @@ func Nano() (int, error) {
 			if update.Sign() < 0 {
 				update = big.NewInt(0)
 			}
-			reps[rep] = update
+			validators[representative] = update
 		}
 
 		// appropriate other people's weight where delegated
 		for account := range group.Accounts {
-			if theft, ok := reps[account]; ok {
+			if theft, ok := validators[account]; ok {
 				power.Add(power, theft)
 			}
-			reps[account] = big.NewInt(0)
+			validators[account] = big.NewInt(0)
 		}
 
-		reps[group.Identity] = power
+		validators[group.Identity] = power
 	}
 
-	type Entity struct {
-		Name   string
-		Weight *big.Int
-	}
-
-	topReps := make([]Entity, 0, len(reps))
-	for rep, control := range reps {
-		topReps = append(topReps, Entity{
-			Name:   rep,
+	actorsByWeight := make([]nanoEntity, 0, len(validators))
+	for validator, control := range validators {
+		actorsByWeight = append(actorsByWeight, nanoEntity{
+			Name:   validator,
 			Weight: control,
 		})
 	}
-	sort.Slice(topReps, func(i, j int) bool {
-		return topReps[i].Weight.Cmp(topReps[j].Weight) > 0
+	sort.Slice(actorsByWeight, func(i, j int) bool {
+		return actorsByWeight[i].Weight.Cmp(actorsByWeight[j].Weight) > 0
 	})
 
 	// Tabulate the nakamoto coefficient
 	nakamoto := 0
 	colluding := big.NewInt(0)
-	for _, entity := range topReps {
+	for _, entity := range actorsByWeight {
 		nakamoto += 1
 		colluding = colluding.Add(colluding, entity.Weight)
 		if colluding.Cmp(needed) > 0 {
