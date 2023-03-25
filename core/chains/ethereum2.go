10f681fa678aab2c9c46890a673cf082351e5d7d
xenowits <kumar.abhishek111b@gmail.com>
2023-03-25 11:30:54 +0530

    fix ethereum NC calculation

--- core/chains/ethereum2.go
+++ core/chains/ethereum2.go
@@ -1,38 +1,22 @@
 package chains
 
 import (
+	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
-	"io/ioutil"
+	"io"
+	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 type Eth2Response struct {
-	Page struct {
-		From int `json:"from"`
-		Size int `json:"size"`
-	} `json:"page"`
-	Total int    `json:"total"`
-	Next  string `json:"next"`
+	Total int `json:"total"`
 	Data  []struct {
-		Id                        string  `json:"id"`
-		IdType                    string  `json:"idType"`
-		TimeWindow                string  `json:"timeWindow"`
-		ValidatorCount            int     `json:"validatorCount"`
-		AvgCorrectness            float64 `json:"avgCorrectness"`
-		AvgInclusionDelay         float64 `json:"avgInclusionDelay"`
-		AvgUptime                 float64 `json:"avgUptime"`
-		AvgValidatorEffectiveness float64 `json:"avgValidatorEffectiveness"`
-		ClientPercentages         []struct {
-			Client     string  `json:"client"`
-			Percentage float64 `json:"percentage"`
-		} `json:"clientPercentages"`
 		NetworkPenetration float64 `json:"networkPenetration"`
 	} `json:"data"`
-}
-
-type Eth2ErrorResponse struct {
-	Detail string `json:"detail"`
 }
 
 // https://api.rated.network/docs#/
@@ -42,22 +26,34 @@ type Eth2ErrorResponse struct {
 // voting powers were not included as the api does not provide this metric
 // and the ui does not display it
 func Eth2() (int, error) {
-	controllingPenetration := .33
-	cumulativePenetration := 0.0
-	nakamotoCoefficient := 1
-	url := ""
+	var (
+		controllingPenetration = .33
+		cumulativePenetration  = 0.0
+		nakamotoCoefficient    = 1
+	)
 
-	url = fmt.Sprintf("https://api.rated.network/v0/eth/operators?window=all&idType=entity&size=100")
-	resp, err := http.Get(url)
+	ctx, cancelFunc := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancelFunc()
+
+	url := fmt.Sprintf("https://api.rated.network/v0/eth/operators?window=all&idType=entity&size=100")
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 	if err != nil {
-		errBody, _ := ioutil.ReadAll(resp.Body)
-		var errResp Eth2ErrorResponse
-		json.Unmarshal(errBody, &errResp)
-		return 0, err
+		log.Println(err)
+		return 0, errors.New("create get request for ethereum")
 	}
-	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
+	// Add authorization header to the request
+	// NOTE: You can get your own API_KEY from https://bit.ly/ratedAPIkeys
+	authToken := fmt.Sprintf("Bearer %s", os.Getenv("RATED_API_KEY"))
+	req.Header.Add("Authorization", authToken)
+
+	resp, err := new(http.Client).Do(req)
+	if err != nil {
+		log.Println(err)
+		return 0, errors.New("get request unsuccessful")
+	}
+
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return 0, err
 	}
@@ -71,8 +67,7 @@ func Eth2() (int, error) {
 	// loop through operators and pull their network penetration
 	// increments NC when the controllingPenetration is surpassed
 	for _, ele := range response.Data {
-		val := ele.NetworkPenetration
-		cumulativePenetration += val
+		cumulativePenetration += ele.NetworkPenetration
 		if cumulativePenetration < controllingPenetration {
 			nakamotoCoefficient += 1
 		}
