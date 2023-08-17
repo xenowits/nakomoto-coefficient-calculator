f099b6a2b618e301a29c81e58993b81c092ee28e
jhernandezb <contact@jhernandez.me>
2023-08-17 09:29:00 -0600

    fix method invokation

--- core/chains/stargaze.go
+++ core/chains/stargaze.go
@@ -2,5 +2,5 @@ package chains
 
 func Stargaze() (int, error) {
 	url := "https://rest.stargaze-apis.com/cosmos/staking/v1beta1/validators?page.offset=1&pagination.limit=500&status=BOND_STATUS_BONDED"
-	return fetchCosmosSDKNakamotoCoefficient("stargaze", url)
+	return fetchCosmosSDKNakaCoeff("stargaze", url)
 }
