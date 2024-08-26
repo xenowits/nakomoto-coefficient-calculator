5d21658fe7befb8f3768f80f8e5fd23a3c9d6c0e
Cordt <Cordt@seifdn.org>
2024-08-26 06:51:46 -0600

    Update regen REST endpoint

--- core/chains/regen.go
+++ core/chains/regen.go
@@ -2,7 +2,7 @@ package chains
 
 func Regen() (int, error) {
 	validatorURL := "https://regen.api.m.stavr.tech/cosmos/staking/v1beta1/validators?page.offset=1&pagination.limit=100&status=BOND_STATUS_BONDED"
-	poolURL := "https://api.regen.aneka.io/cosmos/staking/v1beta1/pool"
+	poolURL := "https://regen.api.m.stavr.tech/cosmos/staking/v1beta1/pool"
 
 	return FetchCosmosSDKNakaCoeff("regen", validatorURL, poolURL)
 }
