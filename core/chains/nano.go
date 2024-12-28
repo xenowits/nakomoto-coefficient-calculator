68c3937ee0d9569af1b615c687cdaf118fa072fb
BrazyDevelopment <lee@lsments.com>
2024-12-28 19:32:03 +0000

    Moved the struct def outside of the function

--- core/chains/nano.go
+++ core/chains/nano.go
@@ -29,8 +29,8 @@ func Nano() (int, error) {
 	defer resp.Body.Close()
 
 	var data NanoStats
-	err = json.NewDecoder(resp.Body).Decode(&data)
-	if err != nil {
+
+	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
 		log.Println("Error decoding JSON:", err)
 		return 0, err
 	}
